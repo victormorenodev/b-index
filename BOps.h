d07e5bd081c27eaa4812eaede53d3b5a1350eeba
Rivanio00 <rivaniofilho04@gmail.com>
2025-05-14 09:12:43 -0300

    refactor: BOps

---
+++ BOps.h
@@ -0,0 +1,27 @@
+#ifndef BOPS_H
+#define BOPS_H   
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include "Structures.h"
+
+using namespace std;
+//buscar
+//inserir == buscar + adicionar chave ao nó
+//calcular altura == descer até achar uma folha, ou armazenar o número de nós e calcular a altura com um equação
+//split == quebra um nó em dois, e adicio a chave mediana ao nó pai
+class BOps {
+    private:
+        static Node readBtreeLine(int line); // ler uma linha da árvore e retorna um node;
+        static void writeBtreeLine(Node node); // escreve um node na árvore;
+        static void splitNode(int line); // splita o nó da linha informada
+        static int countKey(int line); // a partir de uma folha ele conta quantas vezes uma chave se repete na árvore
+    public:
+        static int posKey(int key); // acha a folha que essa chave deve ser inserida, retorna a linha dessa folha
+        static void insertKey(int key, int id, int line); // insere uma chave no nó de linha "line"
+        static int searchKey(int key); // retorna o número de chaves tal que chave == key
+        static int calcHeight(); // calcula a altura da árvore
+};
+
+#endif
